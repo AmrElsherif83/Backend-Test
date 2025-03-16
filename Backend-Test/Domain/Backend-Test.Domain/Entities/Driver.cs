14ec3d0575ccfa47a1b2237491d697d4dbc02872
amr.ahmed <amr.elsherif83@gmail.com>
2025-03-17 00:52:22 +0200

    Implement BaseEntity and Driver Entity Domain layer

---
+++ Backend-Test/Domain/Backend-Test.Domain/Entities/Driver.cs
@@ -0,0 +1,18 @@
+﻿using Backend_Test.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Test.Domain.Entities
+{
+    public class Driver : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+}
